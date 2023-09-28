d37f4f4add571cc30141d1c23f019a682a8b3a19
vonhutrong <vonhutrong@gmail.com>
2023-09-28 23:19:24 +0700

    feat: both dependencies can be used at runtime

--- src/main/java/com/github/vonhutrong/MyMessageDIApplication.java
+++ src/main/java/com/github/vonhutrong/MyMessageDIApplication.java
@@ -1,16 +1,21 @@
 package com.github.vonhutrong;
 
 import com.github.vonhutrong.comsumer.MyDIApplication;
+import com.github.vonhutrong.injectors.EmailServiceInjector;
 import com.github.vonhutrong.injectors.SMSServiceInjector;
 
 public class MyMessageDIApplication {
 
     public static void main(String[] args) {
         String msg = "Hi Trong";
+        String email = "vonhutrong@gmail.com";
         String phone = "0123456789";
 
         MyDIApplication myDIApplication = new MyDIApplication();
         new SMSServiceInjector().inject(myDIApplication);
         myDIApplication.processMessages(msg, phone);
+
+        new EmailServiceInjector().inject(myDIApplication);
+        myDIApplication.processMessages(msg, email);
     }
 }
